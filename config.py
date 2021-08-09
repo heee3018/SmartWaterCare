bfd38f4184b8295e13001eeed70d78741f34c114
heee3018 <heee3018@gmail.com>
2021-08-09 20:48:59 +0900

    Add config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+SERIAL_NUMBER = 'PNC01' # 01 ~ 13(hex)
+
+CHOOSE_ONE_USB = True
+LXC_SERIAL_NUMBER_LIST = []
+
+USE_CSV_SAVE = True
+CSV_SAVE_PATH = ''
+
+USE_DB   = True
+HOST     = 'smartwatercare.net'
+USER     = 'kwater01'
+PASSWORD = '1234'
+DB       = 'kwaterdb'
+TABLE    = 'KWATERTABLE'
+
+USE_REST_API = True
