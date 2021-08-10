55321103a14edb77a33eedb6396ff46eeb5b6752
heee3018 <heee3018@gmail.com>
2021-08-10 10:46:05 +0900

    Add tools/time.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from config import *
+from time import time, sleep
+from datetime import datetime, timedelta
+from config import CHOOSE_ONE_USB, USE_CSV_SAVE, USE_DB
 from tools.print_t import print_t as print
+from tools.check_internet import check_internet
+from tools.time_synchronization import time_sync
+from tools.time import time_format
 
 def init():
     if CHOOSE_ONE_USB:
@@ -14,14 +19,25 @@ def init():
         print('log', 'Use [USE_DB] option')
     else:
         print('warning', '[USE_DB] option is off')
-        
-        
-        
+    
+    if check_internet():
+        print('log', 'Connected to the Internet.')
+        time_sync()
+        print('log', 'Synchronized the time')
+    else:
+        print('warning', 'No internet connection')
+    
+    # devices = list()
+    # devices.append()
+    
     return True
 
 def main():
+    start_time = time()
     while True:
-        pass
+        op_time = time_format(time()-start_time)
+        sleep(60)
+        print('log', f'Operating time: {op_time}')
 
 if __name__ == '__main__':
     try:
