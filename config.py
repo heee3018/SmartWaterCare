08819434ec2cc6785d4b40b508d46a55ead63daa
heee3018 <heee3018@gmail.com>
2021-08-11 18:05:57 +0900

    Modifiy config.py - modify CSV_SAVE_PATH

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 SERIAL_NUMBER = 'PNC01' # 01 ~ 13(hex)
 
+
 CHOOSE_ONE_USB = True
 LXC_SERIAL_NUMBER_LIST = []
 
 USE_CSV_SAVE = True
-CSV_SAVE_PATH = ''
+CSV_SAVE_PATH = '~/Desktop/CSV_Files'
 
 USE_DB   = True
 HOST     = 'smartwatercare.net'
