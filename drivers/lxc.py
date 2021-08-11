0d95aee83a6b3bafc19bc50d20ba051a74b94842
heee3018 <heee3018@gmail.com>
2021-08-11 18:07:05 +0900

    Fix drivers/lxc.py Modifying...

--- drivers/lxc.py
+++ drivers/lxc.py
@@ -1,19 +1,185 @@
 from time      import sleep
 from threading import Thread
 from serial    import Serial, serialutil
-from config    import USE_DB, USE_CSV_SAVE, CHOOSE_ONE_USB
-from config    import FIND_COUNT, SERIAL_NUMBER_LIST
-from config    import HOST, USER, PASSWORD, DB, TABLE 
-from drivers   import database
-from drivers.library import current_time, current_date, save_as_csv, check_internet
-from drivers.library import READ_COMMAND, flip, read_format, to_select_command
-from drivers.library import get_flow_rate, get_total_volume, get_return_serial_num
+from tools.print_t import print_t as print
+from tools.check_internet import check_internet
+from config    import SERIAL_NUMBER
+from config    import USE_CSV_SAVE, CSV_SAVE_PATH
+from config    import USE_DB, HOST, USER, PASSWORD, DB, TABLE 
+from config    import LXC_SERIAL_NUMBER_LIST, CHOOSE_ONE_USB
+from drivers.database import DBSetup
+# from drivers.library  import current_time, current_date, save_as_csv, check_internet
+# from drivers.library  import READ_COMMAND, flip, read_format, to_select_command
+# from drivers.library  import get_flow_rate, get_total_volume, get_return_serial_num
 
+class LXCSetup():
+    def __init__(self, tag, port, interval=0.0):
+        self.name       = 'lxc'
+        self.tag        =  tag
+        self.port       =  port
+        self.interval   =  interval
+        self.serial     =  None
+        self.serial_num =  None
+        self.select_cmd =  None
+        self.data       =  None
+        self.error_code = '00'
+        
+        if not self.connect_db():
+            print('error', '"__init__" -> An error occurred while setup the DB', self.tag)
+            
+        if not self.connect_serial():
+            print('error', '__init__" -> An error occurred while setup the Serial', self.tag)
+
+    def connect_db(self):
+        if check_internet():
+            if USE_DB:
+                try:
+                    self.db = DBSetup(HOST, USER, PASSWORD, DB, TABLE)
+                except:
+                    return 0
+        else:
+            if USE_DB:
+                print('warning', '"connect_db" -> Cannot connect to DB because there is no internet connection.', self.tag)
+        return True
+
+    def connect_serial(self):
+        try:
+            self.serial = Serial(port=self.port, baudrate=2400, parity='E', timeout=1)
+            if not self.serial.is_open:
+                self.serial.open()
+        except:
+            return 0
+        return True
+    
+    def find_thread_start(self):
+        thread = Thread(target=self.find_serial_num, daemon=True)
+        thread.start()
+    
+    def find_serial_num(self):
+        for fliped_serial_num in flip(LXC_SERIAL_NUMBER_LIST):
+            select_command = to_select_command(fliped_serial_num)
+            
+            self.serial.write(select_command)
+            response = self.serial.read(1)
+                        
+            if response == b'\xE5':
+                self.select_cmd = select_command
+                self.serial_num = flip(fliped_serial_num)
+                print('success', f'"find_serial_num" -> Found {self.serial_num}.', self.tag)
+                break
+            if response == b'':
+                print('error', '"find_serial_num" -> Empty response.', self.tag)
+                continue
+            else:
+                continue
+            
+    def check(self):
+        if not self.serial.is_open:
+            print('error', f'"check" -> {self.port} The serial port is closed.', self.tag)
+            return 0
+        
+        
+        return True  
+            
+    def select(self):
+        if self.select_cmd != None:
+            self.serial.write(self.select_cmd)
+            response = self.serial.read(1)
+            
+        if response != b'\xE5': 
+            print('error', '"select" -> Cant select because the response is not "E5".', self.tag)
+            return 0
+        
+        return True
+    
+    def read(self):
+        try:
+            self.serial.write(READ_COMMAND)
+            read_data = self.serial.read(39)
+            # format : b"h!!h\x08\xffr\x15\x13  \x00\x00\x02\x16\x00\x00\x00\x00\x04\x13\x00\x00\x00\x00\x05>\x00\x00\x00\x00\x04m\x17+\xbc'\xe9\x16"
+        except:
+            print('error', '"read" -> An error occurred while executing the Read command.', self.tag)
+            return 0
+        if read_data[-1:] != b'\x16':
+            print('error', '"read" -> Last data in read response is not "16".', self.tag)
+            return 0
+        if read_data == b'':
+            print('error', '"read" -> Read response is empty.', self.tag)
+            return 0
+        if self.serial_num != get_return_serial_num(read_format(read_data, 7, 11)):
+            print('error', '"read" -> Returned serial_number are different.', self.tag)
+            return 0
+        try:
+            self.data = {
+                'time'         : current_time(),
+                'serial_num'   : get_return_serial_num(read_format(read_data, 7, 11)),
+                'flow_rate'    : get_flow_rate(read_format(read_data, 27, 31)),
+                'total_volume' : get_total_volume(read_format(read_data, 21, 25))    }
+        except:
+            print('error', '"read" -> An error occurred while retrieving data.', self.tag)
+            return 0
+        
+        return True
+    
+    def loop_thread_start(self):
+        thread = Thread(target=self.loop, daemon=True)
+        thread.start()
+    
+    def loop(self):
+        READ_NUMBER_OF_ITERATIONS = 10
+        
+        while True:
+            if not self.check(): 
+                print('error', f'"loop" -> {self.port} The serial port is closed.', self.tag)
+                continue
+            if not self.select():
+                print('error', f'"loop" -> Select error occurred.', self.tag)
+                continue
+            else:
+                for _ in range(READ_NUMBER_OF_ITERATIONS):
+                    if not self.read():
+                        print('error', f'"loop" -> Read error occurred.', self.tag)
+                        continue
+                    
+                    sleep(self.interval)   
+                             
+                    time         = self.data['time']
+                    serial_num   = self.data['serial_num']
+                    flow_rate    = self.data['flow_rate']
+                    total_volume = self.data['total_volume']
+
+                    if None in [time, serial_num, flow_rate, total_volume]:
+                        print('error', f'"loop" -> Data contains the value none.', self.tag)
+                        continue
+                    
+                    if USE_CSV_SAVE:
+                        path    = f"csv_files/{current_date()}_{self.serial_num}"
+                        data    = [ time,   serial_num,   flow_rate,   total_volume ]
+                        columns = ['time', 'serial_num', 'flow_rate', 'total_volume']
+                        save_as_csv(device=self.name, data=data, columns=columns, path=path)
+                    # If None is present, it will not be sent to the db.
+                    
+                    if self.use_db:
+                        sql = f"INSERT INTO {self.db.table} (time, serial_num, flow_rate, total_volume) VALUES ('{time}', '{serial_num}', '{flow_rate}', '{total_volume}')"
+                        self.db.send(sql)
+                    
+                    print(f"{'[READ]':>10} {self.tag} - {time} | {serial_num:^12} | {flow_rate:11.6f} ㎥/h | {total_volume:11.6f} ㎥ |")
+                                   
+                                   
+                                   
+                                   
+                                   
+                                   
+                                   
+                                   
+                                   
+        
 class LXC(object):
     def __init__(self, tag, port, interval):
-        self.tag        = tag
-        self.port       = port
-        self.interval   = interval
+        self.name     = 'lxc'
+        self.tag      =  tag
+        self.port     =  port
+        self.interval =  interval
          
         self.state      = 'init'
         self.serial_num =  None
@@ -55,7 +221,7 @@ class LXC(object):
     
     def search_serial_num(self):
         for _ in range(10):
-            for fliped_serial_num in flip(SERIAL_NUMBER_LIST):
+            for fliped_serial_num in flip(LXC_SERIAL_NUMBER_LIST):
                 select_command = to_select_command(fliped_serial_num)
                 try:
                     self.ser.write(select_command)
@@ -157,6 +323,7 @@ class LXC(object):
         
         return True
     
+    
 class Setup(LXC):
     def __init__(self, tag, port, interval=0):
         LXC.__init__(self, tag, port, interval)
@@ -221,175 +388,3 @@ class Setup(LXC):
         #             self.start_read_thread()
         #             break
                     
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-
-# class Setup():
-#     def __init__(self, tag, port):
-#         self.name             = 'lxc'
-#         self.state            = 'init'
-#         self.data             =  { }
-#         self.tag              =  tag
-#         self.serial_port      =  port
-#         self.db               =  database.Setup(HOST, USER, PASSWORD, DB, TABLE)
-#         self.error_cumulative = 0
-        
-#         self.connect_serial()
-
-#     def connect_serial(self):
-#         connect_serial_count = 5
-#         while connect_serial_count > 0:
-#             connect_serial_count -= 1
-        
-#             try:
-#                 self.ser = Serial(port=self.serial_port, baudrate=2400, parity='E', timeout=1)
-#                 if not self.ser.is_open:       
-#                     self.ser.open()
-                
-#                 self.state = 'connected'
-#                 print(f"{'[LOG]':>10} {self.tag} - Successfully opened the port")
-#                 break
-            
-#             except serialutil.SerialException as e:
-#                 if str(e)[:9] == '[Errno 2]':
-#                     print(f"{'[ERROR]':>10} {self.tag} - Could not open port {self.tag}")
-                    
-#                 elif str(e)[:10] == '[Errno 72]':
-#                     print(f"{'[ERROR]':>10} {self.tag} - {str(e)[10:]}")
-                    
-#                 self.state = 'disabled' 
-#                 continue
-            
-#             except OSError:
-#                 print(f"{'[ERROR]':>10} {self.tag} - Protocol error")
-#                 self.state = 'disabled'
-#                 continue
-            
-#     def start_search_thread(self):
-#         thread = Thread(target=self.search_serial_num, daemon=True)
-#         thread.start()
-#         return thread
-    
-#     def search_serial_num(self): 
-#         find_count = FIND_COUNT
-#         while find_count > 0 and self.state == 'connected':
-#             find_count -= 1
-#             print(f"{'[LOG]':>10} {self.tag} - looking for Serial number that matches {self.tag}...")
-#             for reversed_num in flip(SERIAL_NUMBER_LIST):
-#                 select_command = to_select_command(reversed_num)
-#                 self.ser.write(select_command)
-                
-#                 try:
-#                     response = self.ser.read(1)
-#                 except:
-#                     continue
-
-#                 if response == b'\xE5':
-#                     print(f"{'[LOG]':>10} {self.tag} - {flip(reversed_num)} and {self.tag} were successfully matched !")
-#                     self.data[flip(reversed_num)] = {
-#                         'state'          : 'detected',
-#                         'select'         :  select_command,
-#                         'time'           :  current_time(),
-#                         'serial_num'     :  None,
-#                         'flow_rate'      :  None,
-#                         'total_volume'   :  None
-#                     }
-                    
-#                     if CHOOSE_ONE_USB:
-#                         # If you are looking for only one
-#                         self.data = {flip(reversed_num) : self.data[flip(reversed_num)]}
-#                         break 
-                
-#                 else:
-#                     self.data[flip(reversed_num)] = {
-#                         'state'          : 'not detected',
-#                         'select'         :  select_command,
-#                         'time'           :  current_time(),
-#                         'serial_num'     :  None,
-#                         'flow_rate'      :  None,
-#                         'total_volume'   :  None
-#                     }
-
-#             # If at least one 'state' in self.data is 'detected'
-#             if 'detected' in [self.data[key]['state'] for key in list(self.data.keys())]:
-#                 self.state = 'running'
-#                 break 
-            
-#             else:
-#                 print(f"{'[LOG]':>10} {self.tag} - Cannot find Serial Number for {self.tag}")
-#                 self.state = 'not found'
-#                 pass
-                
-#     def start_read_thread(self):
-#         if self.state == 'running':
-#             thread = Thread(target=self.read_data, daemon=True)
-#             thread.start()
-    
-#     def read_data(self):
-#         while True:
-#             for key in list(self.data.keys()):
-#                 select_command = self.data[key]['select']
-#                 self.ser.write(select_command)
-                
-#                 if self.ser.read(1) == b'\xE5':
-#                     repeat = 10
-#                     while repeat > 0:
-#                         repeat -= 1
-                        
-#                         self.ser.write(READ_COMMAND)
-#                         read_data = self.ser.read(39)
-                        
-#                         if read_data == b'':
-#                             print(f"{'[ERROR]':>10} {self.tag} - Eempty response")
-#                             self.error_cumulative += 1
-#                             break
-                        
-#                         try:
-#                             time         = current_time()
-#                             serial_num   = get_return_serial_num(read_format(read_data, 7, 11))
-#                             flow_rate    = get_flow_rate(read_format(read_data, 27, 31))
-#                             total_volume = get_total_volume(read_format(read_data, 21, 25))
-#                         except:
-#                             print(f"{'[ERROR]':>10} {self.tag} - Eempty response")
-#                             self.error_cumulative += 1
-#                             break
-                        
-#                         # Update self.data
-#                         if key == serial_num:
-#                             self.data[key] = {
-#                                 'state'          : 'runnuing',
-#                                 'select'         :  select_command,
-#                                 'time'           :  current_time(),
-#                                 'serial_num'     :  key,
-#                                 'flow_rate'      :  flow_rate,
-#                                 'total_volume'   :  total_volume
-#                             }
-                        
-#                         # Save as csv
-#                         if USE_CSV_SAVE:
-#                             path = f"csv/{current_date()}_{key}"
-#                             data = [time, serial_num, flow_rate, total_volume]
-#                             columns = ['time', 'serial_num', 'flow_rate', 'total_volume']
-#                             save_as_csv(device=self.name, data=data, columns=columnspath=path)
-                        
-#                         # Send to db
-#                         if USE_DB:
-#                             self.db.send(f"INSERT INTO {self.db.table} (time, serial_num, flow_rate, total_volume) VALUES ('{time}', '{serial_num}', '{flow_rate}', '{total_volume}')")
-                        
-#                         print(f"{'[READ]':>10} {self.tag} - {time} | {serial_num:^12} | {flow_rate:11.6f} ㎥/h | {total_volume:11.6f} ㎥ |")
-                                   
-#                 else:
-#                     self.error_cumulative += 1
-                
-#             if self.error_cumulative >= 10:
-#                 print(f"{'[ERROR]':>10} {self.tag} - Error accumulates and restarts..")
-#                 self.connect_serial()
-#                 search_thread = self.start_search_thread()
-#                 search_thread.join()
-#                 self.start_read_thread()
-#                 break
